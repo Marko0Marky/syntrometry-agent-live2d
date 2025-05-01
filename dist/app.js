6d3614546d027344679c291810be1e2249deb42e
mikkimoo9666 <mikkimoo9666@gmail.com>
2025-05-01 15:39:06 +0000

    modified:   dist/app.js modified:   dist/viz-live2d.js modified:   js/app.ts modified:   js/viz-live2d.ts modified:   package-lock.json modified:   package.json

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,12 @@
+import './agent.js';
+import './config.js';
+import './draggablePanels.js';
 import Environment from './environment.js';
+import './syntrometry-core.js';
+import './utils.js';
+import './viz-concepts.js';
+import './viz-live2d.js';
+import './viz-syntrometry.js'; //Added a comma here
 class App {
     constructor(canvas, ctx) {
         this.canvas = canvas;
@@ -15,5 +23,9 @@ class App {
         const agent = {};
         this.environment.addAgent(agent);
     }
+}
+function init() {
+    document.addEventListener('DOMContentLoaded', () => {
+    });
 }
 export default App;
