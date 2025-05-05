70f1ecc1fb8ed7dac3dcb1c3f0f41374ee6760ef
Marko Miloradović <63828839+Marko0Marky@users.noreply.github.com>
2025-05-05 15:24:14 +0200

    Delete dist directory

--- dist/appTypes.js
+++
@@ -1,6 +0,0 @@
-// @ts-nocheck
-/**
- * Type definitions for app.ts
- */
-export {};
-//# sourceMappingURL=appTypes.js.map
--- dist/syntrometry-core.ts
+++
@@ -1,7 +0,0 @@
-// Replace tf.randomUniform with tf.random.uniform
-// Around line 81
-// From:
-// const randomWeights = tf.randomUniform([inputDim, outputDim], -0.1, 0.1);
-
-// To:
-const randomWeights = tf.random.uniform([inputDim, outputDim], -0.1, 0.1);
